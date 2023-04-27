70a7952fd4220df5d1e3ae9a66f0f26b698e73e2
juliapzp <julyanap36@gmail.com>
2023-04-27 17:41:08 -0300

    initial commit

--- curso video python/ex059.py
+++ curso video python/ex059.py
@@ -3,7 +3,8 @@ n2 = int(input('Digite o segundo valor: '))
 opcao = 0
 while opcao != 5:
     print('(1) Somar\n(2) Multiplicar\n(3) Maior\n(4) Novos Números\n(5) Sair do programa')
-    opcao = str(input('Digite sua opção: '))
+    opcao = int(input('Digite sua opção: '))
+    print('-'*20)
     if opcao == 1:
         soma = n1 + n2
         print(f'A soma entre {n1} e {n2} é {soma}.')
